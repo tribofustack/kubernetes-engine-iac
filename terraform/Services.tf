1dd72c5cf489a24aa529045e785fe0e72d4c31f8
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-03-09 15:26:52 -0300

    add project id

--- terraform/Services.tf
+++ terraform/Services.tf
@@ -1,24 +1,29 @@
 resource "google_project_service" "enable_artifact_registry_api" {
+  project                    = var.project_id
   service                    = "artifactregistry.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_service" "enable_cloud_resource_manager_api" {
+  project                    = var.project_id
   service                    = "cloudresourcemanager.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_service" "enable_container_api" {
+  project                    = var.project_id
   service                    = "container.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_service" "enable_servicecontrol_api" {
+  project                    = var.project_id
   service                    = "servicecontrol.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_service" "enable_networksecurity_api" {
+  project                    = var.project_id
   service                    = "networksecurity.googleapis.com"
   disable_dependent_services = true
 }
